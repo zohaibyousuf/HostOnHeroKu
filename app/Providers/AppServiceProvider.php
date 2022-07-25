59f9ecd56ab8f9859b3a8d32a8adc843d0e91323
zohaib <zuhaib.yousuf@jeeny.me>
2022-07-25 07:58:29 +0500

    pushing provider changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,14 +27,14 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        // $language = DB::table('languages')->latest()->first();
-        // if(count($language) > 0){
-        //     $alert_product = DB::table('products')->where('is_active', true)->whereColumn('alert_qty', '>', 'qty')->count();
-        //     \App::setLocale($language->code);
-        //     View::share('settings', DB::table('settings')->latest()->first());
-        //     View::share('language', $language);
-        //     View::share('alert_product', $alert_product);
-        // }
+        $language = DB::table('languages')->latest()->first();
+        if(count($language) > 0){
+            $alert_product = DB::table('products')->where('is_active', true)->whereColumn('alert_qty', '>', 'qty')->count();
+            \App::setLocale($language->code);
+            View::share('settings', DB::table('settings')->latest()->first());
+            View::share('language', $language);
+            View::share('alert_product', $alert_product);
+        }
         
         Schema::defaultStringLength(191);
     }
